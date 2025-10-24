9cc833157211cb262605806e7bacad1ab3212170
muhammadsaqlainramzan <muhammadsaqlainramzan4@gmail.com>
2025-10-24 09:22:26 +0500

    Add files via upload

--- Smallest_Largest_Array_Num.cpp
+++ Smallest_Largest_Array_Num.cpp
@@ -5,18 +5,25 @@ int main()
     int marks[5]={34,54,22,87,77};
     int largest=marks[0];
     int smallest=marks[0];
+    int smallest_index;
+    int largest_index;
+
     for(int i=0;i<5;i++)
     {
       if(marks[i]>largest)
       {
         largest=marks[i];
+        largest_index=i;     
       }
       if(marks[i]<smallest)
       {
         smallest=marks[i];
+        smallest_index=i;
       }
     }
     cout<<"Largest number is:"<<largest<<endl;
-    cout<<"Smallest number is:"<<smallest;
+    cout<<"Index of largest number is:"<<largest_index<<endl;
+    cout<<"Smallest number is:"<<smallest<<endl;
+    cout<<"Index of smallest number of array is:"<<smallest_index;
     return 0;
 }
